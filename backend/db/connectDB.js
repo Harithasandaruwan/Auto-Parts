346d8841cd883216cbc7c817b7ecfc12db707d98
Haritha Sandaruwan <155653525+Harithasandaruwan@users.noreply.github.com>
2024-11-16 20:57:30 +0530

    Create backend functions

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -1,3 +1,12 @@
 import mongoose from "mongoose";
 
-export mongoose 
+export const connectDB = async () => {
+  try {
+    console.log("mongo_url: ", process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI)
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log("Error connection to MongoDB: ", error.message);
+    process.exit(1);
+  }
+}
