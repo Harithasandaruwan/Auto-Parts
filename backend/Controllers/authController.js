0a3aea8b77c33c0424ba9d748b8139798b834dd2
Haritha Sandaruwan <155653525+Harithasandaruwan@users.noreply.github.com>
2024-11-17 21:06:23 +0530

    Update code

---
+++ backend/Controllers/authController.js
@@ -0,0 +1,61 @@
+import { User } from '../Models/User.js';
+import bcryptjs from 'bcryptjs';
+import { generateTokenAndSetCookie } from '../Utils/generateTokenAndSetCookie.js';
+import { sendVerificationEmail } from '../mailTrap/emails.js';
+
+export const signup = async (req, res) => {
+
+    const {email, password, name} = req.body;
+
+    try{
+        if(!email || !password || !name){
+            throw new Error('All fields are required');
+        }
+
+        const userAlreadyExists = await User.findOne({ email });
+
+        console.log("userAlreadyExists", userAlreadyExists);
+
+        if(userAlreadyExists){
+            return res.status(400).json({ success: false, message: 'User already exists' });
+        }
+
+        const hashedPassword = await bcryptjs.hash(password, 10);
+        const verificationToken = Math.floor(100000 + Math.random() * 900000).toString();
+
+        const user = new User({
+            email,
+            password: hashedPassword,
+            name,
+            verificationToken,
+            verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000 //24 hours
+        })
+
+        await user.save();
+
+        // jwt
+        generateTokenAndSetCookie(res, user._id);
+
+        await sendVerificationEmail(user.email, verificationToken);
+
+        res.status(201).json({
+            success: true,
+            message: 'User created successfully',
+            use: {
+                ...user._doc,
+                password: undefined,
+            },
+        })
+
+    } catch (error){
+        res.status(400).json({ success: false, message: error.message });
+    }
+}
+
+export const login = async (req, res) => {
+    res.send('login route');
+}
+
+export const logout = async (req, res) => {
+    res.send('logout route');
+}
