b6a8108b0da1c64b4026f5ee8d131e7ad37ba86d
Haritha Sandaruwan <155653525+Harithasandaruwan@users.noreply.github.com>
2024-11-18 22:55:00 +0530

    Add new components and update code

--- frontend/ui/src/App.jsx
+++ frontend/ui/src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 
+import SignUp from './Components/SignUp';
+import Login from './Components/Login';
+import Home from './Components/Home';
+
 function App() {
   const [count, setCount] = useState(0)
 
