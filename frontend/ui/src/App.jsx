ff54bedbfe4189a0e2b3824b7d281a1fd194c7a9
Haritha Sandaruwan <155653525+Harithasandaruwan@users.noreply.github.com>
2024-11-20 23:28:45 +0530

    Remove unused files and update dependencies

--- frontend/ui/src/App.jsx
+++ frontend/ui/src/App.jsx
@@ -1,24 +1,85 @@
-import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import SignUp from './Components/SignUp';
-import Login from './Components/Login';
-import Home from './Components/Home';
+import SignUp from "./Pages/SignUp";
+import Login from "./Pages/Login";
+import Home from "./Pages/Home";
+import EmailVerification from "./Pages/EmailVerification";
+
+import { Toaster } from "react-hot-toast";
+import { useAuthStore } from "./store/authStore";
+import { useEffect } from "react";
+
+// protect routes that require authentication
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated, user } = useAuthStore();
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (!user.isVerified) {
+    return <Navigate to="/verify-email" replace />;
+  }
+
+  return children;
+};
+
+// redirect authenticated users to the home page
+const RedirectAuthenticatedUser = ({ children }) => {
+  const { isAuthenticated, user } = useAuthStore();
+
+  if (isAuthenticated && user.isVerified) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { isCheckingAuth, checkAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  if (isCheckingAuth) return <LoadingSpinner />;
 
   return (
     <div>
       <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
+        <div className="min-h-screen bg-gradient-to-br from-gray-700 via-gray-800 to-gray-900 flex items-center justify-center relative overflow-hidden">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/signup"
+              element={
+                <RedirectAuthenticatedUser>
+                  <SignUp />
+                </RedirectAuthenticatedUser>
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <RedirectAuthenticatedUser>
+                  <Login />
+                </RedirectAuthenticatedUser>
+              }
+            />
+            <Route path="verify-email" element={<EmailVerification />} />
+          </Routes>
+          <Toaster />
+        </div>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
